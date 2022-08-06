ab8528374efb167356b5b8bd6f1d4435bdb2d63e
Igor Silva <igorpas93@gmail.com>
2022-08-06 16:04:48 -0300

    Added JWT and logger class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
     has_many :foods, class_name: "food", foreign_key: "reference_id"
-    validates :email, :cpfCnpj, uniqueness: true
-    validates :password, confirmation: true
-    validates :password, :password_confirmation, presence: true
     has_secure_password
+    validates :email, :cpfCnpj, uniqueness: true, presence: true
     validates_format_of :email, with: /\A([^\s]+)((?:[-a-z0-9]\.)[a-z]{2,})\z/i
+    validates :password, :password_confirmation, presence: true
+    validates :password, confirmation: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   namespace :api, default: { format: :json} do
     resources :foods
     resources :users
+    post '/auth/login', to: 'auth#login'
+    get '/*a', to: 'application#not_found'
   end
 end
